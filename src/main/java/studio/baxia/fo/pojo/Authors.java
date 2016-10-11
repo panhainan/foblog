d15e87c438c5bc5b708763b3531ba4f94c643e84
FirePan <panhainan@yeah.net>
2016-10-11 13:01:06 +0800

    应用程序对应数据库的实体类

---
+++ src/main/java/studio/baxia/fo/pojo/Authors.java
@@ -0,0 +1,18 @@
+package studio.baxia.fo.pojo;
+
+/**
+ * Created by FirePan on 2016/10/11.
+ * 作者实体信息.
+ */
+public class Authors {
+    private Integer id;
+    private String account;//账户名
+    private String password;//账户密码
+    private String penName;//笔名
+    private String email;//常用邮箱
+    private String profile;//个人介绍
+    private String otherInfo;//其他信息
+    private String verificationCode;//验证码
+    private String verifCodeDeadline;//验证码截止时间
+    private String securityQuestionId;//密保问题id
+}
