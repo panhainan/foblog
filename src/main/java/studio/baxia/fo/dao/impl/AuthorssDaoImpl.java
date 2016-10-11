e9107f18df33b05cb9bc45a67477c9e5b1bd1094
FirePan <panhainan@yeah.net>
2016-10-11 15:50:18 +0800

    dao impl

---
+++ src/main/java/studio/baxia/fo/dao/impl/AuthorssDaoImpl.java
@@ -0,0 +1,10 @@
+package studio.baxia.fo.dao.impl;
+
+import studio.baxia.fo.dao.AuthorsDao;
+
+/**
+ * Created by FirePan on 2016/10/11.
+ */
+public class AuthorssDaoImpl implements AuthorsDao {
+
+}
