968f696f6ef77eb5bcfceab77f684778efc8775a
Panhainan <phnlove@163.com>
2016-11-17 19:21:13 +0800

    。

---
+++ src/main/java/studio/baxia/fo/service/IMessageService.java
@@ -0,0 +1,38 @@
+package studio.baxia.fo.service;
+
+import studio.baxia.fo.pojo.Guest;
+import studio.baxia.fo.pojo.Message;
+
+/**
+ * Created by Pan on 2016/11/16.
+ */
+public interface IMessageService {
+
+
+    /**
+     * 游客评论操作
+     * @param guest
+     * @param message
+     * @return
+     */
+    boolean guestMessage(Guest guest, Message message);
+
+    /**
+     * 通过评论id删除评论
+     *
+     * @param messageId 评论id
+     * @param authorId  操作的作者id
+     * @return
+     */
+    Boolean messageDeleteById(int messageId, int authorId);
+
+    /**
+     * 通过文章id删除相关评论
+     *
+     * @param messageArticleId 文章id
+     * @return
+     */
+    Boolean messageDeleteBy(int messageArticleId);
+
+
+}
